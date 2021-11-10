ede565ddda785fb40a53d83e8724e77ebaef1328
joo <sunminigo@sgrsoft.com>
2021-11-10 16:39:43 +0900

    add img

--- workspace/assets/script/common.js
+++ workspace/assets/script/common.js
@@ -1,9 +1,24 @@
-var Common = function () {
-	var self = this;
-	self.init = function () {
-		console.log('init');
-	}
-	self.init();
-}
+$(()=>{
+	$('.slider').slick({
+		lazyLoad: 'ondemand',
+		infinite: true,
+		dots: true,
+		draggable: true,
+		arrows: true,
+		adaptiveHeight: true,
+		centerMode: true,
+		centerPadding: '10%',
+	})
+	.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+		if (currentSlide !== nextSlide) {
+			$('.slick-center + .slick-cloned').each(function (index, node) {
+				var $node = $(node);
 
-var common = new Common();
+				setTimeout(function () {
+					$node.addClass('slick-current');
+					$node.addClass('slick-center');
+				});
+			});
+		}
+	});
+})
