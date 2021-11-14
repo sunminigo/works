3bc492e670dc14d6490fceb8eed63eff1d5cebdd
sunminigo <sunminigo@gmail.com>
2021-11-14 22:10:16 +0900

    up

--- workspace/assets/script/common.js
+++ workspace/assets/script/common.js
@@ -1,13 +1,15 @@
 $(()=>{
 	$('.slider').slick({
 		lazyLoad: 'ondemand',
-		infinite: true,
+		infinite: false,
 		dots: false,
 		draggable: true,
-		arrows: false,
+		arrows: true,
 		adaptiveHeight: true,
 		centerMode: true,
 		centerPadding: '10%',
+		nextArrow: '<button type="button" class="ph-caret-circle-right slick-next"></button>',
+    prevArrow: '<button type="button" class="ph-caret-circle-left slick-prev"></button>',
 	})
 	.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
 		if (currentSlide !== nextSlide) {
